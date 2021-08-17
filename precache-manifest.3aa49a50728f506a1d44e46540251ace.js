f25aed48e6b0cf98cb8c56501efef816e13b022f
Mattlomet <mattlomet@live.com>
2021-08-17 19:58:10 -0400

    Updates

---
+++ precache-manifest.3aa49a50728f506a1d44e46540251ace.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13577a92c7220f2edb55632da0d52fea",
+    "url": "/karen_portfolio/index.html"
+  },
+  {
+    "revision": "da804bf9870825d3927f",
+    "url": "/karen_portfolio/static/css/main.d7ed120d.chunk.css"
+  },
+  {
+    "revision": "0b0eedd919380d531ad3",
+    "url": "/karen_portfolio/static/js/2.96cc542e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karen_portfolio/static/js/2.96cc542e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da804bf9870825d3927f",
+    "url": "/karen_portfolio/static/js/main.940e7a3d.chunk.js"
+  },
+  {
+    "revision": "a849b73c954525b95e80",
+    "url": "/karen_portfolio/static/js/runtime-main.82a4539b.js"
+  },
+  {
+    "revision": "33045566c72b971e1940c7bf7a8671c2",
+    "url": "/karen_portfolio/static/media/01Director of Marketing.33045566.pdf"
+  },
+  {
+    "revision": "4da82b1cf62a18c96b99d3ebaec801b6",
+    "url": "/karen_portfolio/static/media/01resumepic.4da82b1c.png"
+  },
+  {
+    "revision": "73da0178423c28ab3cab65e1fb5979aa",
+    "url": "/karen_portfolio/static/media/02IT Product Manager Director.73da0178.pdf"
+  },
+  {
+    "revision": "4c958518166f05a18e99a2a4517fbbd2",
+    "url": "/karen_portfolio/static/media/02resumepic.4c958518.png"
+  },
+  {
+    "revision": "89e4a4d8013bf9f461f06c6919d2c64b",
+    "url": "/karen_portfolio/static/media/03CIO Resume.89e4a4d8.pdf"
+  },
+  {
+    "revision": "b1fe073c2b1ecca2dcfc40f1e67fa2c3",
+    "url": "/karen_portfolio/static/media/03resumepic.b1fe073c.png"
+  },
+  {
+    "revision": "01cc7eb787c8e2ce08fdf14b0c81c482",
+    "url": "/karen_portfolio/static/media/logo.01cc7eb7.png"
+  },
+  {
+    "revision": "80ab9be11c6e58056bcbca3172e3fe91",
+    "url": "/karen_portfolio/static/media/profile.80ab9be1.jpg"
+  }
+]);
